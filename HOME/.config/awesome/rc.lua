fa918d8e975374fe4ffda7e705d5ee51dfa9dc79
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-12-11 20:03:57 +0600

    swapped alacritty with kitty

--- HOME/.config/awesome/rc.lua
+++ HOME/.config/awesome/rc.lua
@@ -59,7 +59,7 @@ local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.ge
 beautiful.init(theme_path)
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
+terminal = "kitty"
 filemanager = "thunar"
 browser = "google-chrome-stable"
 editor = os.getenv("EDITOR") or "mousepad"
