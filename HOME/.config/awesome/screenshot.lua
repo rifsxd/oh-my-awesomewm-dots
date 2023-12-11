672633bcfade2d2026982d9bdcc69e99a589fdb1
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-12-11 17:19:02 +0600

    a more refined screenshot util

--- HOME/.config/awesome/screenshot.lua
+++ HOME/.config/awesome/screenshot.lua
@@ -1,42 +1,58 @@
 local awful = require("awful")
 local naughty = require("naughty")
 
-timers = { 5,10 }
 screenshot = os.getenv("HOME") .. "/Pictures/screenshots/$(date +%F_%T).png"
 
 function scrot_full()
-    scrot("scrot " .. screenshot .. " -e 'xclip -selection c -t image/png < $f', scrot_callback", scrot_callback, "Take a screenshot of entire screen")
+    scrot("scrot " .. screenshot, "Took a screenshot of the entire screen")
 end
 
 function scrot_selection()
-    scrot("sleep 0.5 && scrot -s " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'", scrot_callback, "Take a screenshot of selection")
+    scrot("sleep 0.5 && scrot -s " .. screenshot, "Took a screenshot of the selection")
 end
 
 function scrot_window()
-    scrot("scrot -u " .. screenshot .. " -e 'xclip -selection c -t image/png < $f'", scrot_callback, "Take a screenshot of focused window")    
+    scrot("scrot -u " .. screenshot, "Took a screenshot of the focused window")
 end
 
 function scrot_delay()
-    local timestamp = os.date("%F_%T")
-    items={}
-    for key, value in ipairs(timers)  do
-        items[#items+1]={tostring(value) , "scrot -d " .. value .. " " .. os.getenv("HOME") .. "/Pictures/screenshots/" .. timestamp .. ".png -e 'xclip -selection c -t image/png < $f'", "Take a screenshot of delay" }
-    end
-    awful.menu.new(
-    {
-        items = items
+    local prompt = "Choose delay (in seconds):"
+    awful.prompt.run {
+        prompt = prompt,
+        textbox = awful.screen.focused().mypromptbox.widget,
+        exe_callback = function(input)
+            local delay = tonumber(input)
+            if delay then
+                local timestamp = os.date("%F_%T")
+                local cmd = "sleep " .. delay .. " && scrot " .. os.getenv("HOME") .. "/Pictures/screenshots/" .. timestamp .. ".png"
+                scrot(cmd, "Took a screenshot with " .. delay .. " seconds delay")
+            else
+                naughty.notify({
+                    text = "Invalid input for delay!",
+                    timeout = 2.0
+                })
+            end
+        end
     }
-    ):show({keygrabber= true})
-    scrot_callback()
 end
 
-function scrot(cmd , callback, args)
-    awful.util.spawn_with_shell(cmd)
-    callback(args)
+function scrot(cmd, args)
+    local success, _, _, exit_code = awful.spawn.easy_async_with_shell(cmd, function(stdout, stderr, reason, exit_code)
+        if exit_code == 0 then
+            scrot_callback(args)
+        else
+            naughty.notify({
+                text = "Failed to take a screenshot!",
+                timeout = 2.0
+            })
+        end
+    end)
 end
+
+
 function scrot_callback(text)
     naughty.notify({
         text = text,
-        timeout = 0.5
+        timeout = 2.0
     })
 end
