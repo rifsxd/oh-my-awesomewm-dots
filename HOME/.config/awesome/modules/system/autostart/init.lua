cd9244350924c6b40658c4e74239c21d14ec2185
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-12-27 16:53:43 +0600

    fixed wrong config file location for compositor

--- HOME/.config/awesome/modules/system/autostart/init.lua
+++ HOME/.config/awesome/modules/system/autostart/init.lua
@@ -17,7 +17,7 @@ for _, process in ipairs(processesToKill) do
     killProcess(process)
 end
 
-awful.spawn.with_shell('compfy --config ~/.config/awesome/misc/compositor/compfy.conf')
+awful.spawn.with_shell('compfy --config ~/.config/awesome/config/compositor/compfy.conf')
 
 awful.spawn.with_shell('/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1')
 
